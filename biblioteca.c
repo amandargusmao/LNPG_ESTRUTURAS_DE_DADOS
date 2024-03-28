6ca92fea2ba90842d50ba7f19a1fea0c1221e085
amandargusmao <104590590+amandargusmao@users.noreply.github.com>
2024-03-27 21:31:01 -0300

    Add files via upload

---
+++ biblioteca.c
@@ -0,0 +1,21 @@
+typedef struct {
+    char titulo[256];
+    char autor[256];
+    int anoPublicacao;
+    int numCopias;
+} Livro;
+
+void emprestarLivro(Livro *livro) {
+    return 0;
+}
+
+void devolverLivro(Livro *livro) {
+    return 0;
+}
+
+
+void verificarCopiasDisponiveis(const Livro *livro) {
+    return 0;
+}
+
+void obterInformacoesLivro(const Livro *livro) {};
